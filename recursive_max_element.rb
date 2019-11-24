ba4d4d431545709f94f62a96bd4a870f39df6f91
Luis Cairampoma <luis.cairampoma.arbildo@gmail.com>
2019-11-24 12:35:41 -0500

    [ADD] Recursive MAX_ELEMENT function

---
+++ recursive_max_element.rb
@@ -0,0 +1,21 @@
+def max_element(list)
+  if list.length == 0
+    return 0
+  elsif list.length == 1
+    return list.first
+  else
+    if list.first > max_element(list[1..-1])
+      return list.first
+    else
+      return max_element(list[1..-1])
+    end
+  end
+end
+
+my_list = [1, 3, 5, 7, 9]
+
+my_list2 = [3, 5, 7, 9, 1, 100, 2, 4, 55, 12, 99, 100, 12, 12, 12]
+
+puts max_element(my_list) # => 9
+puts max_element(my_list2) # => 100
+
